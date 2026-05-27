c396f3696cf53ce398766539130b4836b38f43d7
Arnoldo Tejeda <isaactejeda27@gmail.com>
2026-05-26 18:00:57 -0600

    ghfhgfhg

--- arch1.cpp
+++ arch1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "D" << std::endl;
+    std::cout << "This is arch1.cpp" << std::endl;
     return 0;
 }
